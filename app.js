7ef9345cf03db30861688f4b3a9e58d6187ba145
Reuben Goodridge <rggiyfe@protonmail.com>
2021-07-24 12:48:17 +0100

    Added food and ability to fee, Added random points for movement

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.mod
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js'
 
+
+// Detecting if page is visible
+let visible
+window.addEventListener('visibilitychange', function () {
+  if (document.visibilityState === 'visible') {
+    visible = true
+  } else {
+    visible = false
+  }
+})
+
+// Get mouse position
+let mouseX
+let mouseY
+window.addEventListener('mousemove', function (e) {
+  mouseX = e.x
+  mouseY = e.y
+})
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
@@ -12,41 +31,72 @@ renderer.setClearColor(0x6a994e)
 var mixer
 var clock = new THREE.Clock()
 
-// Load Phrog Model
+// Load Phrog Models
 const basePhrog = new GLTFLoader()
 var obj
 basePhrog.load('assets/models/Phrogs/base phrog/phrog.gltf', function (gltf) {
   mixer = new THREE.AnimationMixer(gltf.scene)
   obj = gltf.scene
+  scene.add(obj)
   var action = mixer.clipAction(gltf.animations[0])
-  scene.add(gltf.scene)
-  action.play()
+  action.loop = THREE.LoopOnce
+
+  // Play animation randomly every 1-10s.
+  setInterval(() => {
+    action
+      .reset()
+      .play()
+  }, Math.random() * 9000 + 1000)
 })
 
-const stonePhrog = new GLTFLoader()
-var stonePhrogMixer
-var stonePhrogObj
-stonePhrog.load('assets/models/Phrogs/stone phrog/stone-phrog.glb', function (gltf) {
-  stonePhrogMixer = new THREE.AnimationMixer(gltf.scene)
-  stonePhrogObj = gltf.scene
-  var action = stonePhrogMixer.clipAction(gltf.animations[0])
-  scene.add(gltf.scene)
-  stonePhrogObj.position.x -= 5
-  action.play()
-})
+// Create random move points
+var mPoints = []
+var mPointGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+for (var i = 0; i < 20; i++) {
+  var mPoint = new THREE.Mesh(mPointGeometry, new THREE.MeshBasicMaterial({ color: 0xf0f0f0 }))
+  mPoint.position.x = Math.random() * 100 - 50
+  mPoint.position.z = Math.random() * 80 - 40
+  scene.add(mPoint)
+  mPoints.push(mPoint)
+}
 
-const light = new THREE.HemisphereLight(0xffffff, 0x000000, 2)
+// Add Food
+const foodGeometry = new THREE.SphereGeometry()
+const foodColour = new THREE.MeshBasicMaterial({ color: 0xddbea9 })
+const food = new THREE.Mesh(foodGeometry, foodColour)
+food.position.y += 5
+var foodY = food.position.y
+food.userData.draggable = true
+food.userData.name = 'food'
+
+document.getElementById('food').onclick = feed
+let fed = false
+function feed () {
+  if (!fed) {
+    scene.add(food)
+    fed = true
+  } else {
+    window.alert('Your phrog is already fed!')
+  }
+}
+
+const light = new THREE.HemisphereLight(0xa3b18a, 0x3a5a40, 2)
 scene.add(light)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-camera.position.set(5, 5, 5)
+camera.position.set(-2.427074329283623, 23.32717459671259, 56.114230367885504)
 controls.update()
 
 function animate () {
   requestAnimationFrame(animate)
   var delta = clock.getDelta()
-  if (mixer) mixer.update(delta)
-  if (stonePhrogMixer) stonePhrogMixer.update(delta)
+  if (fed && obj.position.y <= foodY) {
+    foodY -= 0.01
+    food.position.y -= 0.01
+  } else {
+    scene.remove(food)
+  }
+  // if (stonePhrogMixer) stonePhrogMixer.update(delta)
   renderer.render(scene, camera)
 }
 animate()
