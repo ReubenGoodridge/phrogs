a382f1a0b36de31572ec6b72d9aa8f6332240c75
Reuben Goodridge <rggiyfe@protonmail.com>
2021-07-28 14:58:31 +0100

    Made movement using Tween.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.mod
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js'
 
-
 // Detecting if page is visible
 let visible
 window.addEventListener('visibilitychange', function () {
@@ -11,14 +10,6 @@ window.addEventListener('visibilitychange', function () {
   } else {
     visible = false
   }
-})
-
-// Get mouse position
-let mouseX
-let mouseY
-window.addEventListener('mousemove', function (e) {
-  mouseX = e.x
-  mouseY = e.y
 })
 
 const scene = new THREE.Scene()
@@ -78,38 +69,64 @@ document.getElementById('food').onclick = feed
 let fed = false
 function feed () {
   if (!fed) {
+    phrogPos.x = phrog.position.x
+    phrogPos.y = phrog.position.y
+    phrogPos.z = phrog.position.z
     let foodPos = mPoints[Math.floor(Math.random() * mPoints.length)]
     if (foodPos === phrogPos) {
       foodPos = mPoints[Math.floor(Math.random() * mPoints.length)]
+      foodPoint.x = foodPos.position.x
+      foodPoint.z = foodPos.position.z
+      console.log('Food at phrog position, moved somewhere new')
     }
     foodPoint.x = foodPos.position.x
     foodPoint.z = foodPos.position.z
     food.position.x = foodPoint.x
     food.position.z = foodPoint.z
     scene.add(food)
-    fed = true
+    // Calculate Midpoint between Phrog and Food
+    const midX = (phrogPos.x + foodPoint.x) / 2
+    const midZ = (phrogPos.z + foodPoint.z) / 2
+    const Midpoint = new THREE.Vector3()
+    Midpoint.x = midX
+    Midpoint.z = midZ
+    Midpoint.y = 50
+    // Create a tween to move to the food
+    var position = { x: phrogPos.x, y: phrogPos.y, z: phrogPos.z }
+    var destination = { x: foodPoint.x, z: foodPoint.z, y: foodPoint.y }
+    // Move to the Midpoint
+    var tween = new TWEEN.Tween(position).to(Midpoint, 2000)
+    tween.easing(TWEEN.Easing.Cubic.Out)
+    tween.onUpdate(function () {
+      phrog.position.x = position.x
+      phrog.position.z = position.z
+      phrog.position.y = position.y
+    })
+    tween.onComplete(function () {
+      phrogPos.x = phrog.position.x
+      phrogPos.y = phrog.position.y
+      phrogPos.z = phrog.position.z
+    })
+    tween.start()
+    // Move to destination
+    var tweenA = new TWEEN.Tween(Midpoint).to(destination, 2000)
+    tweenA.easing(TWEEN.Easing.Cubic.Out)
+    tweenA.onUpdate(function () {
+      phrogPos.x = phrog.position.x
+      phrogPos.y = phrog.position.y
+      phrogPos.z = phrog.position.z
+      phrog.position.x = position.x
+      phrog.position.z = position.z
+      phrog.position.y = position.y
+    })
+    tweenA.onComplete(function () {
+      scene.remove(food)
+    })
+    tweenA.start()
+    tween.chain(tweenA)
+    // fed = true
   } else {
     window.alert('Your phrog is already fed!')
-  }
-}
-
-// Create a simple movement function
-function move (destination) {
-  if (destination.x > phrogPos.x) {
-    phrogPos.x += 0.1
-    phrog.position.x += 0.1
-  }
-  if (destination.x < phrogPos.x) {
-    phrogPos.x -= 0.1
-    phrog.position.x -= 0.1
-  }
-  if (destination.z < phrogPos.z) {
-    phrogPos.z -= 0.1
-    phrog.position.z -= 0.1
-  }
-  if (destination.z > phrogPos.z) {
-    phrogPos.z += 0.1
-    phrog.position.z += 0.1
   }
 }
 
@@ -117,16 +134,13 @@ const light = new THREE.HemisphereLight(0xa3b18a, 0x3a5a40, 2)
 scene.add(light)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-camera.position.set(-2.427074329283623, 23.32717459671259, 70.114230367885504)
-camera.lookAt(phrogPos.x, phrogPos.y, phrogPos.z)
+camera.position.set(-2.427074329283623, 23.32717459671259, 60.114230367885504)
 controls.update()
 
 function animate () {
   requestAnimationFrame(animate)
+  TWEEN.update()
   var delta = clock.getDelta()
-  if (fed && phrogPos !== foodPoint) {
-    move(foodPoint)
-  }
   if (mixer) mixer.update(delta)
   renderer.render(scene, camera)
 }
